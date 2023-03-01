777029865e1a3faafc3f80e185720a28c414f9f1
pagar22 <aaryanpagar@hotmail.com>
2023-03-01 21:42:51 +0100

    checkpoint: sqlite db connection

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
 from app.routers import internal, recipes
+from app.schemas.recipes import Base
+from app.database import engine
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 
 app.include_router(internal.router)
